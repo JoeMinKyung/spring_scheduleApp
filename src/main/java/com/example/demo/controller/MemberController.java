2d786776c6df749fb95d8f52b3062686d1798e51
minkyungCHO <mk981015@naver.com>
2025-02-12 16:26:01 +0900

    feat: 회원 목록 단건 조회 by id

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,15 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.MemberResponseDto;
 import com.example.demo.dto.SignUpRequestDto;
 import com.example.demo.dto.SignUpResponseDto;
 import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +26,14 @@ public class MemberController {
                 );
 
         return new ResponseEntity<>(signUpResponseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponseDto> findById(@PathVariable Long id) {
+
+        MemberResponseDto memberResponseDto = memberService.findById(id);
+
+        return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,9 +1,14 @@
 package com.example.demo.service;
+import com.example.demo.dto.MemberResponseDto;
 import com.example.demo.entity.Member;
 import com.example.demo.dto.SignUpResponseDto;
 import com.example.demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +23,20 @@ public class MemberService {
         Member savedMember = memberRepository.save(member);
 
         return new SignUpResponseDto(savedMember.getId(), savedMember.getUsername(), savedMember.getEmail());
+    }
+
+    public MemberResponseDto findById(Long id) {
+
+        Optional<Member> optionalMember = memberRepository.findById(id);
+
+        // NPE 방지
+        if (optionalMember.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
+        }
+
+        Member findMember = optionalMember.get();
+
+        return new MemberResponseDto(findMember.getUsername(), findMember.getEmail());
     }
 
 }
