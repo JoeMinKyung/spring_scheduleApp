e903da6a206bd5b6a7be9680ce14fa61a71119cb
minkyungCHO <mk981015@naver.com>
2025-02-12 16:18:49 +0900

    feat: SignUp

---
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.SignUpRequestDto;
+import com.example.demo.dto.SignUpResponseDto;
+import com.example.demo.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto requestDto) {
+
+        SignUpResponseDto signUpResponseDto =
+                memberService.signUp(
+                        requestDto.getUsername(),
+                        requestDto.getPassword(),
+                        requestDto.getEmail()
+                );
+
+        return new ResponseEntity<>(signUpResponseDto, HttpStatus.CREATED);
+    }
+
+}
