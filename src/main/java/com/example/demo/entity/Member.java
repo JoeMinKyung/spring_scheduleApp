7b52951b5f5a70fd57475ec532776c344f9b44d4
minkyungCHO <mk981015@naver.com>
2025-02-12 15:15:43 +0900

    feat: Member 구현

--- src/main/java/com/example/demo/entity/Member.java
+++ src/main/java/com/example/demo/entity/Member.java
@@ -1,4 +1,21 @@
 package com.example.demo.entity;
 
-public class Member {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
 }
